52dbb795cec2cee9d35c9305d51ba47df0968b4d
Santfogu <79651297+Santfogu@users.noreply.github.com>
2023-02-12 21:48:09 -0500

    Taller Vistas Django

--- monitoring/settings.py
+++ monitoring/settings.py
@@ -78,11 +78,11 @@ WSGI_APPLICATION = 'monitoring.wsgi.application'
 DATABASES = {
      "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "<nombrebd>",
-        "USER": "<usuario>",
-        "PASSWORD": "<clave>",
+        "NAME": "arquisoftbd",
+        "USER": "santfogu",
+        "PASSWORD": "202111446",
         "HOST": "localhost",
-        "PORT": "",
+        "PORT": "5432",
     }
 }
 
